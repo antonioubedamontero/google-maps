705deab1eb89c13e813bc29bda638557b59f3802
Antonio Úbeda Montero <anubedam@gmail.com>
2023-10-12 08:54:15 +0200

    add map geocoder as a service

--- src/app/pages/geocoder-as-service/geocoder-as-service.component.ts
+++ src/app/pages/geocoder-as-service/geocoder-as-service.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { MapGeocoder } from '@angular/google-maps';
+
 @Component({
   selector: 'app-geocoder-as-service',
   templateUrl: './geocoder-as-service.component.html',
-  styleUrls: ['./geocoder-as-service.component.scss']
+  styleUrls: ['./geocoder-as-service.component.scss'],
 })
 export class GeocoderAsServiceComponent {
-
+  constructor(geocoder: MapGeocoder) {
+    geocoder
+      .geocode({
+        address: '1600 Amphitheatre Parkway, Mountain View, CA',
+      })
+      .subscribe(({ results }) => {
+        console.log(results);
+      });
+  }
 }
